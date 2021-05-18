1752b7a213ef4ab88943c7baf94571362aac2885
Vexter3 <vexter.cz@gmail.com>
2021-05-18 17:22:20 +0200

    1. nahrání souborů

---
+++ index.js
@@ -0,0 +1,10 @@
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+function square(number) {
+    return number * number;
+}
+console.log(square(5));
+greet('Vojtěch', 'Krůpa');
+console.log(document.location);
